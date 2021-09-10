c530ddb738267fedd310a69bc5abe70ff253b7ef
rahimBasha <rahimdev786@gmail.com>
2021-09-10 11:50:14 +0530

    Initial Commit

---
+++ gitTutorials2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTutorials2
+//
+//  Created by shaik Arshad on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
